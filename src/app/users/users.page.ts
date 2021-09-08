52f9d9679e26ed4e4445f75ee69d3bc688a5e97c
ammarRBK <alshakmaskeain@gmail.com>
2021-09-08 15:59:31 +0300

    signup form is ready

--- src/app/users/users.page.ts
+++ src/app/users/users.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MustMatch } from './_helper'
 
 @Component({
   selector: 'app-users',
@@ -7,9 +9,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UsersPage implements OnInit {
 
-  constructor() { }
+  interfaceForm: FormGroup;
+  district: string;
+  block: string;
+  houseHold: string;
+  formvalidate;
+
+  constructor(private signupForm: FormBuilder) { }
 
   ngOnInit() {
+    this.interfaceForm= this.signupForm.group({
+      userName: ['',[Validators.required,Validators.minLength(3)]],
+      phoneNumber: ['',[Validators.required, Validators.minLength(9), Validators.maxLength(10), Validators.pattern(/\-?\d*\.?\d{1,2}/)]],
+      password: ['', [Validators.required, Validators.minLength(8), Validators.pattern(/^([0-9]+[a-zA-Z]+|[a-zA-Z]+[0-9]+)[0-9a-zA-Z]*$/)]],
+      confirmPassword: ['',[Validators.required]],
+      district: ['',[Validators.required]],
+      block: ['',[Validators.required]],
+      houseHold: ['',[Validators.required]]
+    },{validator: MustMatch('password', 'confirmPassword')});
+    this.formvalidate=this.interfaceForm.controls
+  }
+
+  submitSignup(){
+    console.log(JSON.stringify(this.interfaceForm.value))
   }
 
 }
