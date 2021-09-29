165b32368b2bf3e7d192f51274b6e337b13617f8
ammarRBK <alshakmaskeain@gmail.com>
2021-09-29 12:26:13 +0300

    starting with check loggedin feature

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -31,6 +31,12 @@ export class AuthService {
       catchError(this.handleError));
   }
 
+  checkLoggedIn(){
+    return this.http.get(this.url+'users/checkloggedin',this.httpOptions).pipe(
+      retry(2),
+      catchError(this.handleError));
+  }
+
   private handleError(error: HttpErrorResponse) {
     if (error.status === 0) {
       // A client-side or network error occurred. Handle it accordingly.
