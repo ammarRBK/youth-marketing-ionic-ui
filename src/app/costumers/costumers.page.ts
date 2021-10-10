403ad39bddb60700e10dbb78e073784b5553e75d
ammarRBK <alshakmaskeain@gmail.com>
2021-10-10 12:53:23 +0300

    getting productions from server worked succesfully

--- src/app/costumers/costumers.page.ts
+++ src/app/costumers/costumers.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthService } from '../services/auth.service';
+import { ProductsService } from '../services/products.service';
 
 @Component({
   selector: 'app-costumers',
@@ -10,12 +11,20 @@ import { AuthService } from '../services/auth.service';
 export class CostumersPage implements OnInit {
 
   loggedin:boolean;
+  productions: any;
 
-  constructor(private router:Router, private authServ: AuthService) { }
+  constructor(private router:Router, private authServ: AuthService, private products: ProductsService) { }
 
   ngOnInit() {
+    this.getProds();
     this.authServ.checkLoggedIn().subscribe(res => {
       res['message']=== "loggedin" ? this.loggedin= true : this.loggedin= false;
+    })
+  }
+
+  getProds(){
+    this.products.getProducts().forEach((elem:any)=>{
+      this.productions= elem
     })
   }
 
--- src/app/services/products.service.ts
+++ src/app/services/products.service.ts
@@ -1,5 +1,7 @@
-import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpErrorResponse, HttpHeaders } from '@angular/common/http';
+import { throwError, Observable } from 'rxjs';
+import { retry, catchError } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -8,9 +10,32 @@ export class ProductsService {
 
   constructor(private http: HttpClient) { }
   url= 'http://localhost:3000/api/';
+  httpOptions: object={
+    headers: new HttpHeaders({
+      'Access-Control-Allow-Origin':'*',
+      'Content-Type': 'application/json'
+    })
+  }
+
   getProducts(){
-    this.http.get(this.url+"products/getproducts").pipe(
-      
+    return this.http.get(this.url+"products/getproducts",this.httpOptions).pipe(
+      retry(2),
+      catchError(this.handleError)
     )
+  }
+
+  private handleError(error: HttpErrorResponse) {
+    if (error.status === 0) {
+      // A client-side or network error occurred. Handle it accordingly.
+      console.error('An error occurred:', error.error);
+    } else {
+      // The backend returned an unsuccessful response code.
+      // The response body may contain clues as to what went wrong.
+      console.error(
+        `Backend returned code ${error.status}, body was: `, error.error);
+    }
+    // Return an observable with a user-facing error message.
+    return throwError(
+      'Something bad happened; please try again later.');
   }
 }
