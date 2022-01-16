60903cdc1c61523111e7ecb03521e4a1983de336
ammarRBK <alshakmaskeain@gmail.com>
2022-01-16 13:19:14 +0200

    backword hardware button proccess added

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,14 +1,41 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthService } from '../services/auth.service';
-
+import { Platform, AlertController } from '@ionic/angular';
+import { Plugins } from '@capacitor/core';
+const { App } = Plugins;
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-  constructor(private router: Router, private authServ: AuthService) {}
+  constructor(private router: Router, private authServ: AuthService,private alert: AlertController, private platform: Platform) { 
+    this.platform.backButton.subscribe(()=>{
+      this.warningAlert()
+    })
+  }
+
+  async warningAlert(){
+    const alerterOptions= await this.alert.create({
+      message: "!!هل ترغب في الخروج من البرنامج",
+      buttons:[
+        {
+          text: "إلغاء",
+          role: "cancel"
+        },
+        {
+          text: "!!خروج",
+          handler: ()=>{
+
+            App.exitApp();
+          }
+        }
+      ]
+    })
+
+    await alerterOptions.present()
+  }
   
   users(){
     this.authServ.checkLoggedIn().subscribe(res =>{
