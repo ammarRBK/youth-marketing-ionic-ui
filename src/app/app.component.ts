01ebf7e1e13d85bfb28d0988e8b51879d2f6b49a
ammarRBK <alshakmaskeain@gmail.com>
2022-02-20 12:43:15 +0200

    hide splash screen

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { SplashScreen } from '@capacitor/splash-screen';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   constructor() {}
+  ngOnInit(){
+    SplashScreen.hide()
+  }
 }
