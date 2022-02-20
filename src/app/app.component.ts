513ee51e45ab2ce6d6eb5b2f72cdbcf8d4fd7522
ammarRBK <alshakmaskeain@gmail.com>
2022-02-20 14:02:32 +0200

    hide splash screen

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { SplashScreen } from '@awesome-cordova-plugins/splash-screen/ngx';
+import { Platform } from '@ionic/angular';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
 })
-export class AppComponent {
-  constructor(private splashscreen: SplashScreen) {}
+export class AppComponent implements OnInit {
+  constructor(private splashscreen: SplashScreen, private platform: Platform) {}
   ngOnInit(){
-    this.splashscreen.hide();
+    this.platform.ready().then(()=>{
+      this.splashscreen.hide();
+    })
+    
   }
 }
