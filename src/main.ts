fdec0ebdbe6b7ec2d1ea99e2a2a74835f1754a81
ammarRBK <alshakmaskeain@gmail.com>
2023-10-02 13:47:05 +0300

    fix zone.js error by downgrade the version to 0.13.0

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,3 @@
-import { enableProdMode } from '@angular/core';
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
-import { AppModule } from './app/app.module';
-import { environment } from './environments/environment';
-
-import { defineCustomElements } from '@ionic/pwa-elements/loader';
-
-if (environment.production) {
-  enableProdMode();
-}
-
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.log(err));
-  defineCustomElements(window);
+version https://git-lfs.github.com/spec/v1
+oid sha256:0ff8a2cce23e7bd3ea4ca21a608eab5f463937df314f4607c147be0c63f3f797
+size 483
