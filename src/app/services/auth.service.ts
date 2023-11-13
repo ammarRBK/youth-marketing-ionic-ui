78c896dd14016bd9a0d01fc61bc126aea4e3d7ea
marketingYouth <youth.marketing.developer@gmail.com>
2023-11-13 11:09:21 +0300

    fixed costumers page toolbar view

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b17c1f7224744db4c66475ee6968e167e2bc2b3150e21a519ea0a8cb419f6efd
-size 3744
+oid sha256:9287ca2e6f5c6126cd91b166ddee49201560b641b62a69468bbd7dfe4b502ba3
+size 4214
