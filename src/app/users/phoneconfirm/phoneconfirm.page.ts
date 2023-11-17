fce3ad2c5e9895e8b627f20ac4955575bcbe460b
marketingYouth <youth.marketing.developer@gmail.com>
2023-11-17 21:53:51 +0300

    phone confirm first step worked immedietly but need to cotinue and modifications because it contains a bugs

--- src/app/users/phoneconfirm/phoneconfirm.page.ts
+++ src/app/users/phoneconfirm/phoneconfirm.page.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:28512e15d943b2114a45d08bd401d97970973d7bbbce86d08a20f924bf466c96
-size 2559
+oid sha256:f0683e402c1008e426c2aab5a7f7f3394b6cb8b8de49be31b7ea15f1f675c715
+size 2556
