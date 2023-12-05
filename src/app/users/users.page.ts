1d776e7afcbbb8eeaa3f219df4e9a8beb0801ca2
ammarRBK <alshakmaskeain@gmail.com>
2023-12-05 13:10:15 +0300

    fix bug in signup page that don’t display the success message

--- src/app/users/users.page.ts
+++ src/app/users/users.page.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a21a196231ae1bfa31b2b373ea4663afd712c478221b40ee7e6451a7151f3bfc
-size 7977
+oid sha256:fd9e838f3d35ae12732632271e663189df4e3868af542bddb221fe3c55437717
+size 7410
