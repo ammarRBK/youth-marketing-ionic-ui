b7e1a68cd6bd8931d491d75ec4c4ce06a97f5f14
ammarRBK <alshakmaskeain@gmail.com>
2023-12-05 10:52:36 +0300

    trying to focus on first element in phone confirm page

--- src/app/users/phoneconfirm/phoneconfirm.page.ts
+++ src/app/users/phoneconfirm/phoneconfirm.page.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f0683e402c1008e426c2aab5a7f7f3394b6cb8b8de49be31b7ea15f1f675c715
-size 2556
+oid sha256:63d08a71f96e5edd4bf9a3420feeb8e1e54c37030a12ebfd13c98616ad05ec05
+size 2623
--- src/app/users/users.page.ts
+++ src/app/users/users.page.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1ee1cc1d2275d9053b8a6ce287662143cb92646b9259d22b7e4e12525f3924c6
-size 7910
+oid sha256:a21a196231ae1bfa31b2b373ea4663afd712c478221b40ee7e6451a7151f3bfc
+size 7977
