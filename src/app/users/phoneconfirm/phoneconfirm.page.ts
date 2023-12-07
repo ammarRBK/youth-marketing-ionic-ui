936415fd55a77793771d81f70665a513483bf0e7
ammarRBK <alshakmaskeain@gmail.com>
2023-12-07 15:08:09 +0300

    fixed backbutton bug in profile page that go back to users page instead of home page

--- src/app/users/phoneconfirm/phoneconfirm.page.ts
+++ src/app/users/phoneconfirm/phoneconfirm.page.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d7605f7ab302afddf0f274e593a61f4c37de0231ef1f9a90b43b9e15100a435f
-size 3332
+oid sha256:bfc1f491ae29ff808e278172b3e9ed90d89a9525a146aa172b7e51abcd9f0e35
+size 3377
