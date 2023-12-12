0945f73fc7eec142b0af1ee7b020ad5603c4799d
ammarRBK <alshakmaskeain@gmail.com>
2023-12-12 14:56:37 +0300

    handle products date values and send them to server done, now needs some updates on addproduct page UI to control user options with dates

--- src/app/login/profile/addproduct/addproduct.page.ts
+++ src/app/login/profile/addproduct/addproduct.page.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:797a732221d536fefefc733379e76c5915a499ad2a0c5e6bfbbfee506894bf87
-size 11433
+oid sha256:0855c4895c665e9d7fa2abd568450f68ed53aef9e3f8a6bd0b2cd889925520af
+size 7030
