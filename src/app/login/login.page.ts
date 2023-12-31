843f21b074d6f1b87b5dbdb8acea103bfcbd0a30
ammarRBK <alshakmaskeain@gmail.com>
2023-12-31 14:00:26 +0300

    added forgot password alert input for phone number and show code confirm modal

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:23d64b3ca07881f4fd3e8b79dbf6b235316bbe70c9cb4c9acd6ecc987af5245e
-size 3377
+oid sha256:e033711d9f8e22eb37751afb0da4960ee1ec9eb3708d9417aed8a40164b5c26d
+size 5899
--- src/app/users/phoneconfirm/phoneconfirm.page.ts
+++ src/app/users/phoneconfirm/phoneconfirm.page.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0eb30c176f81d396489eb61b27c06e208ca6b91d6c91a9a66f5112d1eb415b97
-size 3855
+oid sha256:0a497fb917d1218577fe58d2ca090c2ba2b2802a8d4a307d1e79968220dcf486
+size 4096
